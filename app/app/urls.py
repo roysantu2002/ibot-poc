2c986eabc9f72c4f901d3aab64274f416fcedb50
“Santu <roysantu2002@yahoo.com>
2022-12-21 14:13:13 +0530

    added token api

--- app/app/urls.py
+++ app/app/urls.py
@@ -3,12 +3,16 @@
 
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework_simplejwt.views import (TokenObtainPairView,
+                                            TokenRefreshView)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     # path('api/', include('api.urls', namespace='api')),
     path('api/account/', include('accounts.urls')),
+    path('api/token', TokenObtainPairView.as_view()),
+    path('token/refresh', TokenRefreshView.as_view()),
     # path('api/account/', include('api.urls')),
 
 
